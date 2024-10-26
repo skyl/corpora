f2b8dd1fc16f15eb2c27fc45745621a69c5e3a00
Skylar Saveland <skylar.saveland@gmail.com>
2024-10-26 20:58:01 +0000

    try to differentiate CI

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Add Zsh history persistence
-echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/home/vscode/.corpora.zsh_history/.zsh_history' >> ~/.zshrc
-zsh
+if [ -z "$CI" ] || [ "$CI" != "true" ]; then
+    echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/home/vscode/.corpora.zsh_history/.zsh_history' >> ~/.zshrc
+    zsh
+fi
