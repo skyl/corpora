e7fd722ed7cdab99eaa8450b765e438e4c918aeb
Skylar Saveland <skylar.saveland@gmail.com>
2024-10-26 19:11:18 +0000

    some cleanup of devcontainer setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install Python requirements
-pip install -r py/requirements.txt
-
 # Add Zsh history persistence
 echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/home/vscode/.corpora.zsh_history/.zsh_history' >> ~/.zshrc
 touch /home/vscode/.corpora.zsh_history/.zsh_history
-# source ~/.zshrc
