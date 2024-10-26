54f01203f1345a75bb2d197e25eacef464b8a668
Skylar Saveland <skylar.saveland@gmail.com>
2024-10-26 19:32:51 +0000

    no need to touch history hopefully

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,4 +2,4 @@
 
 # Add Zsh history persistence
 echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/home/vscode/.corpora.zsh_history/.zsh_history' >> ~/.zshrc
-touch /home/vscode/.corpora.zsh_history/.zsh_history
+# touch /home/vscode/.corpora.zsh_history/.zsh_history
