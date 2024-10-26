72efbd2eef9a9dbb4735894aab5eb43d231c7f69
Skylar Saveland <skylar.saveland@gmail.com>
2024-10-26 21:11:27 +0000

    let's go then

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# If a command is passed, run it (useful for CI).
-if [ $# -gt 0 ]; then
-  echo "RUNNING"
-  exec "$@"
+# # If a command is passed, run it (useful for CI).
+# if [ $# -gt 0 ]; then
+#   echo "RUNNING"
+#   exec "$@"
+# else
+#   # smell
+#   echo "SLEEPING"
+#   git config --global --add safe.directory /workspace
+#   sleep infinity
+# fi
+
+if [ "$CI" = "true" ]; then
+  echo "CI mode: not starting interactive shell."
+  # exit 0  # Do nothing in CI
 else
-  # smell
-  echo "SLEEPING"
+  # Local dev: Drop into Zsh
+  echo "Starting Zsh for local development"
   git config --global --add safe.directory /workspace
+  # exec zsh
   sleep infinity
 fi
