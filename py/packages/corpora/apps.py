634e56cafdb8ad3fe8d3af81ae9f9666c209b539
Skylar Saveland <skylar.saveland@gmail.com>
2024-10-26 21:52:27 +0000

    black

--- py/packages/corpora/apps.py
+++ py/packages/corpora/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CorporaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'corpora'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "corpora"
