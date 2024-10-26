1b5956e62a46a4b2240c0d5bae003ded2551f921
Skylar Saveland <skylar.saveland@gmail.com>
2024-10-26 21:05:10 +0000

    ...

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -2,9 +2,11 @@
 
 # If a command is passed, run it (useful for CI).
 if [ $# -gt 0 ]; then
+  echo "RUNNING"
   exec "$@"
 else
   # smell
+  echo "SLEEPING"
   git config --global --add safe.directory /workspace
   sleep infinity
 fi
