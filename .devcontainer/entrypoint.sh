9b48198e708c74c188e46d5c6cf3cbc23799e828
Skylar Saveland <skylar.saveland@gmail.com>
2024-10-26 14:25:04 -0700

    try this logs thing

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# # If a command is passed, run it (useful for CI).
-# if [ $# -gt 0 ]; then
-#   echo "RUNNING"
-#   exec "$@"
-# else
-#   # smell
-#   echo "SLEEPING"
-#   git config --global --add safe.directory /workspace
-#   sleep infinity
-# fi
-
 if [ "$CI" = "true" ]; then
   echo "CI mode: not starting interactive shell."
   # exit 0  # Do nothing in CI
