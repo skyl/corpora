1cb7e9fd6c9f5384aed42ad08bafc80f4657b58f
Skylar Saveland <skylar.saveland@gmail.com>
2024-10-26 21:01:08 +0000

    hrm

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,11 @@
 
 # Add Zsh history persistence
 if [ -z "$CI" ] || [ "$CI" != "true" ]; then
+    echo "RUNNING zsh with history persistence"
+    echo $CI
     echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/home/vscode/.corpora.zsh_history/.zsh_history' >> ~/.zshrc
     zsh
+else
+    echo "CI is true, skipping zsh with history persistence"
+    echo $CI
 fi
