2c7cb3c31041cf6e9a534a0587c44289a7e4dd49
Skylar Saveland <skylar.saveland@gmail.com>
2024-11-02 01:22:13 +0000

    ..

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,4 @@
 # echo "starting zsh..."
 echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/home/vscode/.corpora.zsh_history/.zsh_history' >> ~/.zshrc
 echo alias tree="tree -I '.venv|node_modules|__pycache__|.git|.pytest_cache' -a" >> ~/.zshrc
-# TODO: corpora_client dev experience has to be 100
-pip install -e /workspace/py/gen/corpora_client
 zsh
