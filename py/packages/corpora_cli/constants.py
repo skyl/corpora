00712429a5dab3e79c8353c19a5d865983904a3e
Skylar Saveland <skylar.saveland@gmail.com>
2024-11-03 00:01:17 +0000

    black

--- py/packages/corpora_cli/constants.py
+++ py/packages/corpora_cli/constants.py
@@ -1,6 +1,7 @@
 from rich.markdown import Markdown
 
-NO_AUTHENTICATION_MESSAGE = Markdown("""
+NO_AUTHENTICATION_MESSAGE = Markdown(
+    """
 Currently we support **Client Credential** authentication.
 
 Check the **server** in your `.corpora.yaml` file and make sure the `base_url` is correct.
@@ -14,4 +15,5 @@ Then, in your CLI environment, set the following environment variables:
 export CORPORA_CLIENT_ID="{your_client_id}"
 export CORPORA_CLIENT_SECRET="{your_client_secret}"
 ```
-""")
+"""
+)
