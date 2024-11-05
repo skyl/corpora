fefdd9f232a229bbbb54904d99b151955e08f0f3
Skylar Saveland <skylar.saveland@gmail.com>
2024-11-05 00:39:11 +0000

    run server automatically and start new terminal

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,4 +4,4 @@
 # echo "starting zsh..."
 echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/home/vscode/.corpora.zsh_history/.zsh_history' >> ~/.zshrc
 echo alias tree="tree -I '.venv|node_modules|__pycache__|.git|.pytest_cache' -a" >> ~/.zshrc
-zsh
+# zsh
