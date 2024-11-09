26692ee2a4a1f1eca0c3043398db4c281b31db8a
Skylar Saveland <skylar.saveland@gmail.com>
2024-11-09 19:42:51 +0000

    add tests for corpustextfile by path

--- py/packages/corpora/routers/corpustextfile.py
+++ py/packages/corpora/routers/corpustextfile.py
@@ -13,28 +13,6 @@ from ..auth import BearerAuth
 file_router = Router(tags=["file"], auth=BearerAuth())
 
 
-@file_router.post(
-    "", response={201: FileResponseSchema, 409: str}, operation_id="create_file"
-)
-@async_raise_not_found
-async def create_file(request, payload: FileSchema):
-    """Create a new File within a Corpus."""
-    corpus = await Corpus.objects.aget(id=payload.corpus_id)
-    checksum = compute_checksum(payload.content)
-    try:
-        file = await CorpusTextFile.objects.acreate(
-            path=payload.path,
-            content=payload.content,
-            checksum=checksum,
-            corpus=corpus,
-        )
-    except IntegrityError:
-        # Handle the unique constraint violation for duplicate file paths within the same corpus
-        raise HttpError(409, "A file with this path already exists in the corpus.")
-
-    return 201, file
-
-
 # Get file by path, given a corpus ID
 @file_router.get(
     "/corpus/{corpus_id}", response=FileResponseSchema, operation_id="get_file_by_path"
@@ -59,3 +37,25 @@ async def get_file(request, file_id: uuid.UUID):
     """Retrieve a File by ID."""
     file = await CorpusTextFile.objects.select_related("corpus").aget(id=file_id)
     return file
+
+
+@file_router.post(
+    "", response={201: FileResponseSchema, 409: str}, operation_id="create_file"
+)
+@async_raise_not_found
+async def create_file(request, payload: FileSchema):
+    """Create a new File within a Corpus."""
+    corpus = await Corpus.objects.aget(id=payload.corpus_id)
+    checksum = compute_checksum(payload.content)
+    try:
+        file = await CorpusTextFile.objects.acreate(
+            path=payload.path,
+            content=payload.content,
+            checksum=checksum,
+            corpus=corpus,
+        )
+    except IntegrityError:
+        # Handle the unique constraint violation for duplicate file paths within the same corpus
+        raise HttpError(409, "A file with this path already exists in the corpus.")
+
+    return 201, file
