bafb2b30653f9f6b2d6c389099159dadd6052df5
Skylar Saveland <skylar.saveland@gmail.com>
2024-11-11 19:26:23 +0000

    compress VOICE

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/home/vscode/.corpora.zsh_history/.zsh_history' >> ~/.zshrc
-echo "alias tree=\"tree -I '\\.venv|node_modules|__pycache__|\\.git|\\.pytest_cache' -a\"" >> ~/.zshrc
+echo "alias tree=\"tree -I '\\.venv|node_modules|build|dist|test-corpora|__pycache__|\\.git|\\.pytest_cache' -a\"" >> ~/.zshrc
 sudo ln -s /workspace/py/packages/corpora_cli/main.py /usr/local/bin/corpora
