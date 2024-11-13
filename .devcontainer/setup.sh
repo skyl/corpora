a24f61eb7e1d58a735a89a7a10a20741367c49de
Skylar Saveland <skylar.saveland@gmail.com>
2024-11-13 03:49:44 +0000

    zsh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,4 +4,3 @@ echo 'autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd
 echo "alias tree=\"tree -I '\\.venv|node_modules|build|dist|test-corpora|__pycache__|\\.git|\\.pytest_cache' -a\"" >> ~/.zshrc
 echo "alias git-delete-branches=\"git branch | grep -v 'main' | xargs git branch -d\"" >> ~/.zshrc
 sudo ln -s /workspace/py/packages/corpora_cli/main.py /usr/local/bin/corpora
-sudo chsh -s /bin/zsh vscode
